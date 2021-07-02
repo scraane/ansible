7a2c6f849ef140d8119f53c2b565dccf34193832
Stephan Craane <stephan.craane@gmail.com>
2021-07-02 16:07:28 +0200

    .

--- sh/centos.sh
+++ sh/centos.sh
@@ -4,9 +4,9 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-yum update -y
-yum install python3 python3-pip gnupg2 -y
-yum install git -y
+dnf update -y
+dnf install python3 python3-pip gnupg2 -y
+dnf install git -y
 pip3 install ansible
 ansible --version
 ansible-pull -U https://github.com/scraane/ansible.git
