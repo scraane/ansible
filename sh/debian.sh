ed2e54455ff44207b0d2cefc5215a0e511f24b35
nutjob <stephan.craane@gmail.com>
2021-11-10 11:57:04 +0100

    Update debian.sh

--- sh/debian.sh
+++ sh/debian.sh
@@ -8,6 +8,6 @@ apt update -y
 apt upgrade -y
 apt install python3 python3-pip gnupg2 -y
 apt install git -y
-pip3 install ansible docker-py
+pip3 install ansible
 ansible --version
 ansible-pull -U https://github.com/scraane/ansible.git
